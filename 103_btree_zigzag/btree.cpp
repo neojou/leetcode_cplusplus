e6d36708214f856759d46798bc38f46f5fc44bca
neojou <neojou@gmail.com>
2021-10-12 14:33:31 +0800

    Leetcode 103 binary tree zigzag level order traversal
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ 103_btree_zigzag/btree.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+template <typename T>
+void print_vector(vector<T> const &v)
+{
+    cout << "[";
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+	if (it != v.begin())
+	    cout << ", ";
+	cout << *it;
+    }
+    
+    cout << "]";
+}
+
+template <typename T>
+void print_vector2(vector<vector<T>> const &v)
+{
+    cout << "[";
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+	if (it != v.begin())
+	    cout << ", ";
+	print_vector(*it);
+    }
+    
+    cout << "]" << endl;
+}
+
+/* ListNode */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void print_ListNode(ListNode* h)
+{
+    cout << "[";
+
+    if (h != nullptr) {
+        cout << h->val;
+
+    	for (ListNode *it = h->next; it != nullptr; it = it->next)
+	    cout << " ," << it->val;
+    }
+    
+    cout << "]";
+}
+
+bool list_comp(ListNode *l1, ListNode *l2, ListNode *end) {
+    while (l1 != end) {
+        if (l1->val != l2->val)
+            return false;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    return true;
+}
+
+ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *next;
+        
+        while (head != nullptr) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+}
+
+/* TreeNode */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+string &print_TreeNode1(string &s, TreeNode* t)
+{
+    if (t == nullptr)
+        return s;
+
+    s += to_string(t->val);
+    s += ", ";
+
+    if (t->left)
+	print_TreeNode1(s, t->left);
+
+    if (t->right)
+	print_TreeNode1(s, t->right);
+
+    return s;
+}
+ 
+void print_TreeNode(TreeNode* t)
+{
+    cout << "[";
+    string s = "";
+    s = print_TreeNode1(s, t);
+    cout << s.substr(0, s.size() - 2) << "]" << endl;
+}
+
+class Solution {
+private:
+     vector<vector<int>> &zigzagLevelOrder1(vector<vector<int>> &vvi, TreeNode* root, int level, bool zag) {
+        if (root == nullptr)
+            return vvi;
+
+        if (level >= vvi.size()) {
+            vector<int> vi = {root->val};
+            vvi.push_back(vi);
+        } else {
+            vector<int> &vi = vvi[level];
+            if (zag)
+                vi.insert(vi.begin(), root->val);
+            else
+                vi.push_back(root->val);
+        }
+        
+        if (root->left)
+            zigzagLevelOrder1(vvi, root->left, level + 1, !zag);
+        
+        if (root->right)
+            zigzagLevelOrder1(vvi, root->right, level + 1, !zag);
+        
+        return vvi;
+    }
+
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> vvi = {};
+        
+        if (root == nullptr)
+            return vvi;
+
+        vector<int> vi = {root->val};
+        vvi.push_back(vi);
+        
+        if (root->left)
+            zigzagLevelOrder1(vvi, root->left, 1, true);
+        
+        if (root->right)
+            zigzagLevelOrder1(vvi, root->right, 1, true);
+        
+        return vvi;
+    }
+};
+
+int main() {
+    Solution s;
+    TreeNode *root = new TreeNode(3,
+			new TreeNode(9),
+			new TreeNode(20,
+				new TreeNode(15),
+				new TreeNode(7)));
+
+    vector<vector<int>> output = s.zigzagLevelOrder(root);
+    cout << "Output: ";
+    print_vector2(output);
+
+    return 0;
+}
+
