4cbece44bd99f843b502b7bf4df86777d36d7326
neojou <neojou@gmail.com>
2021-10-13 21:52:58 +0800

    Leetcode 232 implement queue using stacks
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ 232_implementqueue_using_stacks/queue.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+template <typename T>
+void print_vector(vector<T> const &v)
+{
+    cout << "[";
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+	if (it != v.begin())
+	    cout << ", ";
+	cout << *it;
+    }
+    
+    cout << "]";
+}
+
+template <typename T>
+void print_vector2(vector<vector<T>> const &v)
+{
+    cout << "[";
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+	if (it != v.begin())
+	    cout << ", ";
+	print_vector(*it);
+    }
+    
+    cout << "]" << endl;
+}
+
+class MyQueue {
+    stack<int> s1, s2;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int ret = s2.top();
+        s2.pop();
+        
+        return ret;
+    }
+    
+    int peek() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        return s2.top();
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+int main() {
+    MyQueue *obj = new MyQueue();
+
+    obj->push(1);
+    obj->push(2);
+    cout << obj->peek() << endl;
+    cout << obj->pop() << endl;
+    cout << ((obj->empty()) ? "true" : "false") << endl;
+
+    return 0;
+}
+
