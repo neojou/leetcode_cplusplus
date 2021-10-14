07243df0cfa269a9d3c71b2fefa07c5f7cc9cee0
neojou <neojou@gmail.com>
2021-10-14 17:46:26 +0800

    Leetcode 24 swap nodes in pairs
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ 24_swap_nodes_in_pairs/listnode.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+template <typename T>
+void print_vector(vector<T> const &v)
+{
+    cout << "[";
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+	if (it != v.begin())
+	    cout << ", ";
+	cout << *it;
+    }
+    
+    cout << "]";
+}
+
+template <typename T>
+void print_vector2(vector<vector<T>> const &v)
+{
+    cout << "[";
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+	if (it != v.begin())
+	    cout << ", ";
+	print_vector(*it);
+    }
+    
+    cout << "]" << endl;
+}
+
+/* ListNode */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void print_ListNode(ListNode* h)
+{
+    cout << "[";
+
+    if (h != nullptr) {
+        cout << h->val;
+
+    	for (ListNode *it = h->next; it != nullptr; it = it->next)
+	    cout << " ," << it->val;
+    }
+    
+    cout << "]";
+}
+
+bool list_comp(ListNode *l1, ListNode *l2, ListNode *end) {
+    while (l1 != end) {
+        if (l1->val != l2->val)
+            return false;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    return true;
+}
+
+ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *next;
+        
+        while (head != nullptr) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+}
+
+class Solution {
+private:
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode *first = head->next;
+        ListNode *second = head;
+        second->next = swapPairs(head->next->next);
+        first->next = second;
+        head = first;
+        
+        return head;
+    }
+};
+
+int main() {
+    Solution s;
+    ListNode *head = new ListNode(1,
+			new ListNode(2,
+			    new ListNode(3,
+				new ListNode(4))));
+
+    ListNode *output = s.swapPairs(head);
+    cout << "Output: ";
+    print_ListNode(output);
+    cout << endl;
+
+    return 0;
+}
+
