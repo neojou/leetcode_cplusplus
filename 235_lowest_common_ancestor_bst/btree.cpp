0e55da65c0aa3ea643a993afa1f13f19d58a4ceb
neojou <neojou@gmail.com>
2021-10-19 17:59:46 +0800

    Leetcode 235 Lowest Common Ancestor of a Binary Search Tree
    Signed-off-by: neojou <neojou@gmail.com>

---
+++ 235_lowest_common_ancestor_bst/btree.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+template <typename T>
+void print_vector(vector<T> const &v)
+{
+    cout << "[";
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+	if (it != v.begin())
+	    cout << ", ";
+	cout << *it;
+    }
+    
+    cout << "]" << endl;
+}
+
+/* ListNode */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void print_ListNode(ListNode* h)
+{
+    cout << "[";
+
+    if (h != nullptr) {
+        cout << h->val;
+
+    	for (ListNode *it = h->next; it != nullptr; it = it->next)
+	    cout << " ," << it->val;
+    }
+    
+    cout << "]";
+}
+
+bool list_comp(ListNode *l1, ListNode *l2, ListNode *end) {
+    while (l1 != end) {
+        if (l1->val != l2->val)
+            return false;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    return true;
+}
+
+ListNode* reverseList(ListNode* head) {
+        ListNode *prev = nullptr;
+        ListNode *next;
+        
+        while (head != nullptr) {
+            next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+}
+
+/* TreeNode */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+string &print_TreeNode1(string &s, TreeNode* t)
+{
+    if (t == nullptr)
+        return s;
+
+    s += to_string(t->val);
+    s += ", ";
+
+    if (t->left)
+	print_TreeNode1(s, t->left);
+
+    if (t->right)
+	print_TreeNode1(s, t->right);
+
+    return s;
+}
+ 
+void print_TreeNode(TreeNode* t)
+{
+    cout << "[";
+    string s = "";
+    s = print_TreeNode1(s, t);
+    cout << s.substr(0, s.size() - 2) << "]" << endl;
+}
+
+// Time: O(h)
+// Space: O(1)
+
+class Solution {
+    
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q)
+            return root;
+
+        auto s = min(p->val, q->val);
+        auto b = max(p->val, q->val);
+        
+        int val = root->val;
+        while (root->val < s || root->val > b)
+            root = (root->val > b)? root->left : root->right;
+        
+        return root;
+    }
+};
+
+int main() {
+    Solution s;
+    TreeNode *p = new TreeNode(2,
+			new TreeNode(0),
+			new TreeNode(4,
+			    new TreeNode(3),
+			    new TreeNode(5)));
+    TreeNode *q = new TreeNode(8,
+			new TreeNode(7),
+			new TreeNode(9));
+
+    TreeNode *root = new TreeNode(6, p, q);
+
+
+    TreeNode *output = s.lowestCommonAncestor(root, p, q);
+    cout << "Output: " << output->val << endl;
+
+    return 0;
+}
+
